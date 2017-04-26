061cc0fc46facc967a205db6f5bb89aa2f047a38
hizegi <ohchristineyi@gmail.com>
2017-04-26 16:28:22 -0400

    adding patch route for messages

--- routes/messages.js
+++ routes/messages.js
@@ -40,4 +40,37 @@ router.post('/', function(req, res, next){
   });
 });
 
+//change existing data
+//put will overwrite existing data
+router.patch('/:id', function(req, res, next){
+  Message.findById(req.params.id, function(err, message){
+    if (err) {
+      return res.status(500).json({
+        title: 'An error occured',
+        error: err
+      })
+    }
+    //if message is not found
+    if (!message) {
+      return res.status(500).json({
+        title: 'No message found.',
+        error: {message: 'Message not found'}
+      })
+    }
+    message.content = req.body.content;
+    message.save(function(err, results){
+      if (err) {
+        return res.status(500).json({
+          title: 'An error occured',
+          error: err
+        })
+      }
+      res.status(200).json({
+        message: 'Updated message',
+        obj: result
+      });
+    });
+  });
+});
+
 module.exports = router;
