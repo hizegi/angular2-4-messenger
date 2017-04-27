370f481eabec1cff2768f6c459e70ed66598cb42
hizegi <ohchristineyi@gmail.com>
2017-04-27 16:12:29 -0400

    added middleware to Message model (delete from User after deleting message)

--- models/message.js
+++ models/message.js
@@ -1,9 +1,20 @@
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
+var User = require('./user')
+
 
 var schema = new Schema({
     content: {type: String, required: true},
     user: {type: Schema.Types.ObjectId, ref: 'User'}
+});
+
+//Mongoose middleware
+//after message is removed remove from User
+schema.post('remove', function(message){
+  User.findById(message.user, function(err, user){
+    user.messages.pull(message);
+    user.save();
+  })
 });
 
 module.exports = mongoose.model('Message', schema);
