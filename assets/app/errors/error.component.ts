2389dd3e5292031c62e19149736561c6dc5a021d
hizegi <ohchristineyi@gmail.com>
2017-05-01 14:27:38 -0400

    adding to error component

---
+++ assets/app/errors/error.component.ts
@@ -0,0 +1,21 @@
+import { Component } from "@angular/core";
+import { Error } from "./error.model";
+
+@Component({
+  selector: 'app-error',
+  templateUrl: './error.component.html',
+  styles: [`
+    .backdrop{
+      background-color: rgba(0,0,0,0.6);
+      position: fixed;
+      top: 0;
+      left: 0;
+      width: 100%;
+      height: 100vh;
+    }
+    `]
+})
+export class ErrorComponent{
+  error: Error
+  displayed = 'none';
+}
